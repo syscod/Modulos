5bed1329ab15d6d5b76077689062095ee680c1cc
edtegomez <edtegomez@gmail.com>
2013-06-13 13:41:57 -0500

    Modulo de Cartera V1

---
+++ academy/__openerp__.py
@@ -0,0 +1,18 @@
+{
+        "name" : "academy",
+        "version" : "0.1",
+        "author" : "Tiny",
+        "website" : "",
+        "category" : "Unknown",
+        "description": """  """,
+        "depends" : ['base'],
+        "init_xml" : [ ],
+        "demo_xml" : [ ],
+        "update_xml" : [
+            'academy_view.xml',
+            'academy_wizard.xml',
+            'wizard/wizard_academy.xml',
+            'workflow/academy_workflow.xml'
+        ],
+        "installable": True
+}
